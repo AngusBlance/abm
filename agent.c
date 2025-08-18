8b607dbf33f24905e835e334145c56652ed208a4
AngusBlance <145984386+AngusBlance@users.noreply.github.com>
2025-08-18 14:02:35 +0000

    enum to str func implemented

--- agent.c
+++ agent.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 typedef enum{
-    CIVILIAN,
-    RIOTER
+    ACTIVE,
+    INACTIVE
 }AgentState;
 
 typedef struct{
@@ -13,11 +13,13 @@ typedef struct{
     int y;
 }Agent;
 
+const char* enum_to_str(AgentState state);
+
 void create_agents(Agent agents[], int num_agents) {
     for (int i = 0; i < num_agents; i++) {
-        agents[i].state = (i % 2 == 0) ? CIVILIAN : RIOTER;  // alternating states
-        agents[i].likelihood = 0.1f * (i + 1);              // 0.1, 0.2, ...
-        agents[i].grievance = 0.2f * (i + 1);               // 0.2, 0.4, ...
+        agents[i].state = (i % 2 == 0) ? ACTIVE : INACTIVE;
+        agents[i].likelihood = 0.1f * (i + 1);
+        agents[i].grievance = 0.2f * (i + 1);
         agents[i].x = i;
         agents[i].y = i * 2;
     }
@@ -28,14 +30,22 @@ void print_agent(Agent *agents, int agent_id, int num_agents){
 
     if (agent_id == -1){
         for (int agent_num = 0; agent_num < num_agents; agent_num++){
-            printf("ID: %.2f\n", agents[agent_num].grievance);
+            printf("gerievance: %.2f\n", agents[agent_num].grievance);
             printf("Likelihood: %.2f\n", agents[agent_num].likelihood);
-            printf("State: %c\n", agents[agent_num].state);
+            printf("State: %s\n", enum_to_str(agents[agent_num].state));
             printf("Position: (%d, %d)\n", agents[agent_num].x, agents[agent_num].y);
+            printf("\n");
         }
     }
 }
 
+const char* enum_to_str(AgentState s){
+    switch(s){
+        case ACTIVE: return "A";
+        case INACTIVE: return "I";
+        default: return "UNKNOWN";
+    }
+}
 
 int main(){
     Agent agents[5];
@@ -47,16 +57,3 @@ int main(){
     print_agent(agents, agent_id, num_agents);
     return 0;
 }
-
-// void print_agent(Agent *agents, int agent_id){
-//     int num_agents = sizeof(agents) / sizeof(agents[0]);
-
-//     if (agent_id == -1){
-//         for (int agent_num; agent_num < num_agents; agent_num++){
-//             printf("ID: %.2f\n", agents[agent_num].grievance);
-//             printf("Likelihood: %.2f\n", agents[agent_num].likelihood);
-//             printf("State: %c\n", agents[agent_num].state);
-//             printf("Position: (%d, %d)\n", agents[agent_num].x, agents[agent_num].y);
-//         }
-//     }
-// }
